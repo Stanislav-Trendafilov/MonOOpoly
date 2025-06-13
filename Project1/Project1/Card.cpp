c0d93d255adf67abf67b2562a3977535bf34049d
Stanislav Trendafilov <114089296+Stanislav-Trendafilov@users.noreply.github.com>
2025-06-13 12:52:24 +0300

    Add_Card

--- Project1/Project1/Card.cpp
+++ Project1/Project1/Card.cpp
@@ -1,1 +1,19 @@
 #include "Card.h"
+
+Card::Card(const MyString& name, const MyString& effect)
+{
+	this->name = name;
+	this->effect = effect;
+};
+
+MyString Card::getName() const
+{
+	return name;
+}
+
+MyString Card::getEffect() const
+{
+	return effect;
+}
+
+
--- Project1/Project1/Card.h
+++ Project1/Project1/Card.h
@@ -1,5 +1,20 @@
 #pragma once
+#include "MyString.h"
+#include "Player.h"
+
 class Card
 {
+	MyString name;
+	MyString effect;
+public:
+	Card() = default;
+	Card(const MyString& name, const MyString& effect);
+	MyString getName() const;
+	MyString getEffect() const;
+
+	virtual void applyEffect(Player& player) = 0;
+
+	virtual Card* clone() const = 0;
+	virtual ~Card() = default;
 };
 
